7c96266b4f8c42b647ab33bb2938b08de5f9cb01
Adrian Wolny <wolny101@gmail.com>
2016-10-20 01:28:08 +0200

    Fix FtpUploader init

--- ftp/ftp_uploader.py
+++ ftp/ftp_uploader.py
@@ -4,25 +4,29 @@ from ftplib import FTP
 
 
 class FtpUploader(threading.Thread):
-
     TMP_DIR = '/tmp/'
 
-    def __init__(self, files, host, user, passwd, dir=None, logger=None):
+    def __init__(self, files, host, user, passwd, ftp_dir=None, logger=None):
         super().__init__()
         self.daemon = True
         self.files = files
-        self.ftp = FTP(host, user=user, passwd=passwd)
-        if dir:
-            self.ftp.cwd(dir)
+        self.host = host
+        self.user = user
+        self.passwd = passwd
+        self.ftp_dir = ftp_dir
         self.logger = logger or logging.getLogger()
 
     def run(self):
         try:
+            ftp = FTP(self.host, user=self.user, passwd=self.passwd)
+            if self.ftp_dir:
+                ftp.cwd(self.ftp_dir)
+
             for filename in self.files:
-                rsp_code = self.ftp.storbinary("STOR " + filename, open(FtpUploader.TMP_DIR + filename, 'rb'))
+                rsp_code = ftp.storbinary("STOR " + filename, open(FtpUploader.TMP_DIR + filename, 'rb'))
                 self.logger.info('FTP upload %s: %s' % (filename, rsp_code))
         except:
             self.logger.error('Unexpected error', exc_info=True)
         finally:
-            self.ftp.close()
-
+            if ftp:
+                ftp.close()
