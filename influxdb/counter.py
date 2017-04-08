f2602722c7d1268d645fa5273e988a50a833dc6a
Adrian Wolny <wolny101@gmail.com>
2017-04-09 01:37:03 +0200

    Fix duplicate self arg

--- influxdb/counter.py
+++ influxdb/counter.py
@@ -1,7 +1,7 @@
 from influxdb import InfluxDBClient
 
 class Counter:
-    def __init__(self, host, port, dbname, self):
+    def __init__(self, host, port, dbname):
         self.client = InfluxDBClient(host=host, port=port, database=dbname)
 
     def increment(self, name, value):
