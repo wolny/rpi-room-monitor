12b70c523eba299f10e326bdfc14471b724abc6b
Adrian Wolny <wolny101@gmail.com>
2017-04-09 13:11:42 +0200

    Default log level: INFO

--- monitor.py
+++ monitor.py
@@ -13,7 +13,7 @@ handler = logging.StreamHandler()
 formatter = logging.Formatter('%(asctime)s %(name)-10s %(levelname)-8s %(message)s')
 handler.setFormatter(formatter)
 logger.addHandler(handler)
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.INFO)
 
 # parse config
 parser = argparse.ArgumentParser(description='Find intruders in my room')
