6010b583cc502228d9e1e69dc2411bdad0ed43cc
Adrian Wolny <wolny101@gmail.com>
2017-04-09 13:07:27 +0200

    Revert "Add Influxdb counter"
    This reverts commit 02358e5049ce5d8daf888c4d42cc6aef07657cea.

--- monitor.py
+++ monitor.py
@@ -39,7 +39,7 @@ detector = motion.MotionDetector(resolution, framerate, logger=logger)
 detector.start()
 
 # create influxdb client
-counter = Counter(config['influxdb_host'], config['influxdb_port'], config['influxdb_db'])
+counter = Counter('192.168.0.16', 8086, 'pimonitor')
 # create frame processor
 frame_processor = frameproc.FrameProcessor(config, counter, logger)
 
