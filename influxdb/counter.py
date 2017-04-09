a30523148b063f3471da56f814f5d26d3cc4931d
Adrian Wolny <wolny101@gmail.com>
2017-04-09 13:07:11 +0200

    Revert "Fix duplicate self arg"
    This reverts commit f2602722c7d1268d645fa5273e988a50a833dc6a.

--- influxdb/counter.py
+++ influxdb/counter.py
@@ -1,7 +1,7 @@
 from influxdb import InfluxDBClient
 
 class Counter:
-    def __init__(self, host, port, dbname):
+    def __init__(self, host, port, dbname, self):
         self.client = InfluxDBClient(host=host, port=port, database=dbname)
 
     def increment(self, name, value):
